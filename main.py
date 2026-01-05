127c3cf5a58b5be5983047c038c79366c9217cd8
Alina Khursa <alinakhursa02@gmail.com>
2026-01-05 08:17:37 -0600

    class Participant is created

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from sqlmodel import Session, select
+import secrets
+import string
+
+from db import create_db_and_tables, get_session
+from models import Event, Participant
+
 
 app = FastAPI()
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return HTMLResponse(content="<h1>Welcome to the Speed Friending application!</h1>")
+    return "<h1>Welcome to the Speed Friending application!</h1>"
+
+class EventCreate(BaseModel):
+    title: str
+class JoinRequest(BaseModel):
+    email: str
+
+
+def generate_join_code(n: int = 6) -> str:
+    alphabet = string.ascii_uppercase + string.digits
+    return "".join(secrets.choice(alphabet) for _ in range(n))
+
 @app.post("/events")
+def create_event(payload: EventCreate, session: Session = Depends(get_session)):
+    join_code = generate_join_code()
+    while session.exec(select(Event).where(Event.join_code == join_code)).first():
+        join_code = generate_join_code()
+
+    event = Event(title=payload.title, join_code=join_code)
+    session.add(event)
+    session.commit()
+    session.refresh(event)
+    return event
+@app.on_event("startup")
+def on_startup():
+    import models  # гарантируем регистрацию таблиц
+    create_db_and_tables()
--- models.py
+++ models.py
@@ -10,5 +10,10 @@ class Event(SQLModel, table=True):
     title: str
     join_code: str = Field(index=True, unique=True)
     timezone: str =  Field(default="Europe/Minsk")
-
     created_at: datetime = Field(default_factory=lambda: datetime.now(Minsk_tz))
+
+class Participant(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    event_id: int = Field(index=True, foreign_key="event.id")
+    email: str = Field(index=True)
+    joined_at: datetime = Field(default_factory=lambda: datetime.now(Minsk_tz))
