675bb134a056d9480ebe3220d53bca0b0482f3f7
Alina Khursa <alinakhursa02@gmail.com>
2026-01-05 05:52:41 -0600

    database file is created

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello, Gomel!")
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+@app.get("/")
+def read_root():
+    return HTMLResponse(content="<h1>Welcome to the Speed Friending application!</h1>")
